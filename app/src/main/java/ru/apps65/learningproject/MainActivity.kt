eccfcce3dfb594daf663b02073fcf4584cfe5a6a
nmodelov <nmodelov@65apps.com>
2021-01-12 12:13:27 +0700

    инициализация проекта

---
+++ app/src/main/java/ru/apps65/learningproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.apps65.learningproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
