0da576e232faabc7614833bb8221aec7127c811a
nmodelov <nmodelov@65apps.com>
2021-01-14 16:27:41 +0700

    hilt impl

--- app/src/main/java/ru/apps65/learningproject/detailcharacter/di/CharacterDetailModule.kt
+++ app/src/main/java/ru/apps65/learningproject/detailcharacter/di/CharacterDetailModule.kt
@@ -2,31 +2,18 @@ package ru.apps65.learningproject.detailcharacter.di
 
 import dagger.Binds
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
 import ru.apps65.learningproject.detailcharacter.data.CharacterDetailRepositoryImpl
 import ru.apps65.learningproject.detailcharacter.domain.repository.CharactersDetailRepository
 import ru.apps65.learningproject.detailcharacter.presentation.CharacterDetailPresenter
 import ru.apps65.learningproject.detailcharacter.presentation.Presenter
-import ru.apps65.learningproject.di.scope.PerPresenter
 
-// Так тоже можно!
 
-//@Module
-//class CharacterDetailModule {
-//
-//    @PerPresenter
-//    @Provides
-//    fun providePresenter(repository: CharactersDetailRepository, router: Router): Presenter =
-//        CharacterDetailPresenter(repository, router)
-//
-//    @Provides
-//    fun provideRepository(): CharactersDetailRepository =
-//        CharacterDetailRepositoryImpl()
-//}
-
+@InstallIn(FragmentComponent::class)
 @Module
 abstract class CharacterDetailModule {
 
-    @PerPresenter
     @Binds
     abstract fun providePresenter(presenter: CharacterDetailPresenter): Presenter
 
